8871b1c9a1d035407c47bf6d884b8397a32f5958
RUDEALTS <flyingbaily@gmail.com>
2022-10-15 15:57:08 +0200

    cooldown

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ async def on_command_error(ctx, error):
         await ctx.send(embed= embed)
         await ctx.message.delete()
     if isinstance(error, commands.CommandOnCooldown):
-        em = discord.Embed(colour=discord.Colour.red(), description=f"Can\'t mention everyone, pls try again in {round(error.retry_after, 2)}.")
+        em = discord.Embed(colour=discord.Colour.red(), description=f"Can\'t mention everyone, pls try again in Try again {time.time() + error.retry_after:.0f}.")
         await ctx.send(embed=em)
 
 
